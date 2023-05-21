028e27802395e1fd4d39bbdaef5e91e7f691ff57
nishattandra <nishattandra2001@gmail.com>
2023-05-21 18:17:32 +0600

    delete toy added

--- index.js
+++ index.js
@@ -96,12 +96,12 @@ async function run() {
         //     res.send(result);
         // })
 
-        // app.delete('/coffee/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await coffeeCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/deletetoy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
     // Send a ping to confirm a successful connection
