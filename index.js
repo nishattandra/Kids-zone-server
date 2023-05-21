909943c7a2819b76eb463c0a8c6b443d1ea83f43
nishattandra <nishattandra2001@gmail.com>
2023-05-21 19:38:36 +0600

    vercel added

--- index.js
+++ index.js
@@ -19,13 +19,22 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    client.connect((err)=>{
+      if(err){
+        console.log(err)
+        return;
+      }
+    });
     // Add Toy
     const toyCollection = client.db('toyStore').collection('toy');
         //All Toys
